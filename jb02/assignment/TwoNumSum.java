92a2c0e7d4ac5553fb0ab9933edfa9838b50dcb9
bean <dbher.0010@gmail.com>
2024-07-22 23:57:23 +0900

    중첩 조건문 부분 리팩토링 완료

--- jb02/assignment/TwoNumSum.java
+++ jb02/assignment/TwoNumSum.java
@@ -2,30 +2,26 @@ package jb02.assignment;
 
 public class TwoNumSum {
     public static void main(String[] args) {
-        if (args.length == 2) {
-            int firstNum = Integer.parseInt(args[0]);
-            int secondNum = Integer.parseInt(args[1]);
-
-            if (firstNum != secondNum) {
-                int big;
-                int small;
-                int sum = 0;
-                if (firstNum > secondNum) {
-                    big = firstNum;
-                    small = secondNum;
-                } else {
-                    big = secondNum;
-                    small = firstNum;
-                }
-                for (int i = small; i <= big; i++) {
-                    sum += i;
-                }
-                System.out.println(firstNum + " ~ " + secondNum + "의 합은 " + sum + "입니다.");
-            } else {
-                System.out.println("입력값이 같습니다. 서로 다른 두 수를 입력해주세요.");
-            }
-        } else {
+        if (args.length != 2) {
             System.out.println("두 수를 입력해주세요.");
+            return;
         }
+
+        int firstNum = Integer.parseInt(args[0]);
+        int secondNum = Integer.parseInt(args[1]);
+
+        if (firstNum == secondNum) {
+            System.out.println("입력값이 같습니다. 서로 다른 두 수를 입력해주세요.");
+            return;
+        }
+
+        int max = firstNum > secondNum ? firstNum : secondNum;
+        int min = firstNum > secondNum ? secondNum : firstNum;
+        int sum = 0;
+
+        for (int i = min; i <= max; i++) {
+            sum += i;
+        }
+        System.out.println(firstNum + " ~ " + secondNum + "의 합은 " + sum + "입니다.");
     }
 }
