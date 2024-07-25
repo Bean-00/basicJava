6a95028d7c3cb0f204d9ed708a413534fd036908
bean <dbher.0010@gmail.com>
2024-07-25 23:42:10 +0900

    jb04 코드 추가

--- jb04/part07/outer/Son1.java
+++ jb04/part07/outer/Son1.java
@@ -1,10 +1,10 @@
 package jb04.part07.outer;
 
-public class Son extends jb04.part07.Father {
-    public Son() {
+public class Son1 extends jb04.part07.Father {
+    public Son1() {
         System.out.println(this.name);
         System.out.println(bank);
-        System.out.println(branch);
+//        System.out.println(branch);
 //        System.out.println(password);
 
         System.out.println(this.getBranch());
