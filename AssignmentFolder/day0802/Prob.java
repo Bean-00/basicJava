409596b106d08d05f5bfb894011c98c950261f29
bean <dbher.0010@gmail.com>
2024-08-02 19:41:03 +0900

    0801 과제 완료

---
+++ AssignmentFolder/day0802/Prob.java
@@ -0,0 +1,18 @@
+package AssignmentFolder.day0802;
+
+public class Prob {
+    private static String[] stringSplit (String str1, String str2){
+        result = str1.split(str2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = "PROD-001#X-note#Samsung#3#6000000";
+        String[] strs = stringSplit(str, "#");
+        System.out.println("=====문자열 처리 결과=====");
+        for (int i = 0; i < strs.length; i++){
+            System.out.println(strs[i]);
+        }
+    }
+}
