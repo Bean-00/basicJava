ec32ebbdd2779b85629993a0d738ee6223cb6611
unknown <dbher.0010@gmail.com>
2024-08-06 21:02:50 +0900

    과제 완료

--- AssignmentFolder/day0806/Prob2.java
+++ AssignmentFolder/day0806/Prob2.java
@@ -1,42 +1,49 @@
-package AssignmentFolder.day0806;
+//package AssignmentFolder.day0806;
 
 public class Prob2 {
     String toEncoding(String decodedStr) {
-        String encodedStr = "홍길";
-        StringBuilder sb = new StringBuilder(16);
-        byte[] temp = decodedStr.getBytes();
-        StringBuilder resultsb = null;
-
+        StringBuilder sb = new StringBuilder();
         char[] decodedArray = decodedStr.toCharArray();
+        byte[] checkAscii = decodedStr.getBytes();
+		
+		if (decodedStr.length() == checkAscii.length)
+		{
+			return decodedStr;
+		}
 
-        for (int i = 0; i < decodedArray.length; i++) {
-            for (char alphabet : decodedArray) {
-                if (alphabet > 127) {
-                    byte[] tempAlpha = Character.toString(alphabet).getBytes();
-                    System.out.println(Integer.toHexString(tempAlpha[0]));
-                    for (byte b : tempAlpha) {
-                        System.out.println("b " + Integer.toHexString(b));
-//                        resultsb.append(Integer.toHexString(b).replace("ffffff", " "));
-                    }
-                    System.out.println(resultsb);
-                } else {
-                    sb.append(alphabet);
-                }
-            }
-            resultsb.append(sb);
-        }
-        return resultsb.toString();
-
+		for (int i = 0; i < decodedArray.length; i++) {
+	
+            byte[] temp = (Character.toString(decodedArray[i])).getBytes();
+               if (temp.length > 1) {
+                   sb.append((Integer.toHexString(temp[0]).toString()
+                		   + Integer.toHexString(temp[1]).toString()).replace("ffffff", "%"));
+               } else {
+                   sb.append(decodedArray[i]);
+               }
+         }
+		return sb.toString();
+            
     }
 
     public static void main(String[] args) {
         Prob2 p = new Prob2();
-        String value = "홍";
-        byte[] bytes = value.getBytes();
-        for (int i = 0; i < bytes.length; i++) {
-            System.out.println("인 " + Integer.toHexString(bytes[i]));
-        };
-        System.out.println(p.toEncoding("홍"));
+        
+        System.out.println("입력: KOREA");
+        System.out.println("출력: " + p.toEncoding("KOREA"));
+        
+        System.out.println("입력: 홍길동");
+        System.out.println("출력: " + p.toEncoding("홍길동"));
+        
+        System.out.println("입력: 홍 길 동");
+        System.out.println("출력: " + p.toEncoding("홍 길 동"));
+        
+        System.out.println("입력: Hong 길 동");
+        System.out.println("출력: " + p.toEncoding("Hong 길 동"));
+        
+        System.out.println("입력: 0319");
+        System.out.println("출력: " + p.toEncoding("0319"));
+        
+        
 
 
 
